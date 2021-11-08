819010edaa459c0672aa74fe971b03724da0ac65
saqib010 <010saqib@gmail.com>
2021-11-08 19:34:06 +0500

    integrated image default for portfolios items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,4 +10,14 @@ class Portfolio < ApplicationRecord
 
 	scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Rubby on Rails')}
 		#ruby_on_rails_portfolio_items  is just a var
+
+	#define defaults values for images 
+	#for every new record creation in portfolio the following items are going to set as defaults.
+	after_initialize :set_defaults
+	def set_defaults
+		self.main_image ||= 'https://via.placeholder.com/600x400'
+		self.thumb_image ||= 'https://via.placeholder.com/350x250'
+	end
+
+	# || means if main image has already value do not override it else if nil set to the default values
 end
